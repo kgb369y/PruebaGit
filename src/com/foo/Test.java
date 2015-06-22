2bdaecab76718f7e5a6203c1450175d3edbdbabd
kbedoya <kgb369y@gmail.com>
2015-06-22 14:53:40 -0300

    bla

--- src/com/foo/Test.java
+++ src/com/foo/Test.java
@@ -12,69 +12,57 @@ import org.testng.asserts.SoftAssert;
 
 public class Test {
 
-    String browser = "firefox";
-    String siteToGo = "http://www.google.com";
-    public static WebDriver driver;
+	String browser = "firefox";
+	String siteToGo = "http://www.google.com";
+	public static WebDriver driver;
 
-    @BeforeTest
-    public void beforeTest() throws Exception {
-	runDrivers(browser);
-	driver.get(siteToGo);
-    }
-
-    @AfterTest
-    public void afterTest() {
-	closeBrowser(browser);
-    }
-
-    @org.testng.annotations.Test(groups={"grupo1"})
-    public void test1() throws Exception {
-	SoftAssert sA = new SoftAssert();
-	try {
-	    sA.assertTrue(true);
-	    sA.assertAll();
-	} catch (AssertionError e) {
-	    throw e;
+	@BeforeTest
+	public void beforeTest() throws Exception {
+		runDrivers(browser);
+		driver.get(siteToGo);
 	}
-    }
 
-    @org.testng.annotations.Test(groups={"grupo1"})
-    public void test2() throws Exception {
-	SoftAssert sA = new SoftAssert();
-	try {
-	    sA.assertTrue(false);
-	    sA.assertAll();
-	} catch (AssertionError e) {
-	    throw e;
+	@AfterTest
+	public void afterTest() {
+		closeBrowser(browser);
 	}
-    }
-	
-	@org.testng.annotations.Test(groups={"grupo2"})
-    public void test3() throws Exception {
-	SoftAssert sA = new SoftAssert();
-	try {
-	    sA.assertTrue(true);
-		sA.assertTrue(false);
-	    sA.assertAll();
-	} catch (AssertionError e) {
-	    throw e;
-	}
-    }
 
-    public void runDrivers(String browser) throws IOException {
-	WebDriver driver;
-	driver = DriverSetUp.getDriver(browser);
-	Test.driver = driver;
-    }
-
-    public static void closeBrowser(String browser) {
-	try {
-	    if (browser.contains("ie") || browser.contains("IE")) {
-		driver.quit();
-	    } else
-		driver.close();
-	} catch (Exception e) {
-	    System.out.println(e);
+	@org.testng.annotations.Test(groups = { "grupo1" })
+	public void test1() throws Exception {
+		SoftAssert sA = new SoftAssert();
+		try {
+			sA.assertTrue(true);
+			sA.assertAll();
+		} catch (AssertionError e) {
+			throw e;
+		}
 	}
-    }
+
+	@org.testng.annotations.Test(groups = { "grupo1" })
+	public void test2() throws Exception {
+		SoftAssert sA = new SoftAssert();
+		try {
+			sA.assertTrue(false);
+			sA.assertAll();
+		} catch (AssertionError e) {
+			throw e;
+		}
+	}
+
+	public void runDrivers(String browser) throws IOException {
+		WebDriver driver;
+		driver = DriverSetUp.getDriver(browser);
+		Test.driver = driver;
+	}
+
+	public static void closeBrowser(String browser) {
+		try {
+			if (browser.contains("ie") || browser.contains("IE")) {
+				driver.quit();
+			} else
+				driver.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }
